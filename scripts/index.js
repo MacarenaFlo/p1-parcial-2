2af9cf4fb9160d6eea93b66bbe4d331d7d90dd2d
Maca Flo <182832922+MacarenaFlo@users.noreply.github.com>
2025-06-28 14:43:40 -0300

    creacion de documento html con dom

--- scripts/index.js
+++ scripts/index.js
@@ -15,52 +15,63 @@ document.addEventListener("DOMContentLoaded", () => {
             productos.forEach(producto => {
 
             const card = document.createElement("div");
-            card.setAttribute("class", "card col-8 mb-4");
-            card.setAttribute("style", "width: 18rem;");
+            card.setAttribute("class", "card mb-4");
+           
             card.dataset.id = producto.id;
 
             const cardBody = document.createElement("div");
             cardBody.setAttribute("class", "card-body");
 
             const nombreProducto = document.createElement("h3");
-            nombreProducto.setAttribute("class", "card-title");
-            nombreProducto.setAttribute("style", "font-size: 1.5rem;");
+            nombreProducto.setAttribute("class", "nombre-producto");
             nombreProducto.innerText = producto.nombre;
 
             const imagen = document.createElement("img");
             imagen.setAttribute("class", "card-img-top");
             imagen.setAttribute("src", producto.imagen);
             imagen.setAttribute("alt", producto.nombre);
-            imagen.setAttribute("style", "width: 100%; height: 200px; object-fit: cover;");
+            
 
             const descripcionProducto = document.createElement("p");
-            descripcionProducto.setAttribute("class", "card-text");
-            descripcionProducto.setAttribute("style", "font-size: 1.2rem;");
+            descripcionProducto.setAttribute("class", "card-text text-truncate"); 
             descripcionProducto.innerText = producto.descripcion;
+            
+            const botonVerMas = document.createElement("a");
+            botonVerMas.setAttribute("class", "detalle");
+            botonVerMas.setAttribute("style", "width: 100%;");
+            botonVerMas.innerText = "Ver más";
+            botonVerMas.addEventListener("click", () => {
+                mostrarDetallesProducto(producto.id);
+                
+            });
+
 
             const categoria = document.createElement("p");
-            categoria.setAttribute("class", "card-text");
-            categoria.setAttribute("style", "font-size: 1.2rem; font-weight: bold;");
+            categoria.setAttribute("class", "categoria");
             categoria.innerText =  producto.categoria;
 
             const precioProducto = document.createElement("p");
             precioProducto.setAttribute("class", "card-text");
-            precioProducto.setAttribute("style", "font-size: 1.2rem;");
+            precioProducto.setAttribute("style", "font-weight: bold; color: #f706ae; font-size: 2em; text-align: end");
+
             precioProducto.innerText = `$${producto.precio}`;
 
             const botonAgregar = document.createElement("button");
-            botonAgregar.setAttribute("class", "btn btn-primary");
+            botonAgregar.setAttribute("class", "btn");
             botonAgregar.setAttribute("style", "width: 100%;");
             botonAgregar.innerText = "Agregar al carrito";
             botonAgregar.addEventListener("click", () => {
                 agregarAlCarrito(producto.id);
             });
 
+            cardBody.appendChild(categoria);
             cardBody.appendChild(nombreProducto);
             cardBody.appendChild(imagen);
             cardBody.appendChild(descripcionProducto);
             cardBody.appendChild(precioProducto);
+            cardBody.appendChild(botonVerMas);
             cardBody.appendChild(botonAgregar);
+            
             card.appendChild(cardBody);
             document.querySelector("#productos").appendChild(card);
         });
