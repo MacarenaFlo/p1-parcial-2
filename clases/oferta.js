92bbb24883b96f375011bc0812c4f6de14befdf5
Maca Flo <182832922+MacarenaFlo@users.noreply.github.com>
2025-08-02 18:52:31 -0300

    ofertas

---
+++ clases/oferta.js
@@ -0,0 +1,11 @@
+"use strict";
+
+class Oferta {
+    constructor(id, categoria, titulo, descripcion, detalle) {
+        this.id = id;
+        this.categoria = categoria;
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        this.detalle = detalle;
+    }
+}
